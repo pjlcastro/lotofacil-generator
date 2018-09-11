63ef231976d44c5508c8f988b481375cb3569ad9
paulo.jose <paulo.jose@leitedecastro.com.br>
2018-09-11 05:14:28 +0000

    Removed array creation

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ var app = express();
 var generatorics = require('generatorics');
 
 var redis = require('redis');
-redis.debug_mode = true;
+//redis.debug_mode = true;
 
 app.get('/', function (req, res) {
     var client = redis.createClient(6379, 'lotofacil');
     
-    //var client = redis.createClient(80, 'lotofacil-pleitede.b542.starter-us-east-2a.openshiftapps.com')
+    //var client = redis.createClient(80, 'lotofacil-pleitede.b542.starter-us-east-2a.openshiftapps.com');
+    
     client.auth('BDRSVE350Dipojtj');
     
     client.on('connect', function() {
@@ -16,7 +17,7 @@ app.get('/', function (req, res) {
         var combinations = generatorics.combination(['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25' ], 15);
         console.log('Inserting games into database...');
         
-        var arrayOfCombinations = Array.from(combinations);
+        //var arrayOfCombinations = Array.from(combinations);
         
         var counter = 0;
         var control = 0;
@@ -29,7 +30,7 @@ app.get('/', function (req, res) {
             for (var dezen of combination) {
                 fullGame += dezen;
             }
-            console.log(combination);
+            console.log(fullGame.toString());
             batch.sadd('games', fullGame.toString());
             counter++;
             control++;
